8737815e197a6d97ac296f4a66745edc7566e0f4
JQiue <861947542@qq.com>
2024-03-12 15:06:22 +0800

    feat: add feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
+#[cfg(feature = "rand")]
 pub mod rand {
     use rand::Rng;
-    use std::ops::Range;
-    pub fn generate_random_between_limits(range: Range<i32>) -> i32 {
-        rand::thread_rng().gen_range(range)
+
+    pub fn generate_random_between_limits(min: i32, max: i32) -> i32 {
+        rand::thread_rng().gen_range(min..=max)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::rand::generate_random_between_limits;
+        #[test]
+        fn it_works() {
+            println!("{}", generate_random_between_limits(1, 10));
+        }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::rand::generate_random_between_limits;
-    #[test]
-    fn it_works() {
-        println!("{}", generate_random_between_limits(1..10));
-    }
-}
+#[cfg(feature = "jwt")]
+pub mod jwt {}
